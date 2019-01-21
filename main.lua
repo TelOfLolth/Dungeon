497f72efcf79513aa95383ef3998b013ea30a2b7
TelOfLolth <28494487+TelOfLolth@users.noreply.github.com>
2019-01-21 09:01:34 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,47 +1,47 @@
 function _init()
-	player = {}
-	player.x = 60
-	player.y = 100
-	player.sprite = 1
-	player.speed = 1
+	p = {}
+	p.x = 60
+	p.y = 100
+	p.sprite = 1
+	p.speed = 1
 	
-	enemy = {}
+	e = {}
 end
 
 function _update()
 -- Code for movement and collision checks
 	if btn(0) then
-		if(solid_tile(player.x-1, player.y) == false) then
-			player.x -= player.speed
+		if(solid_tile(p.x-1, p.y) == false) then
+			p.x -= p.speed
 		end
 	end
 
 	if btn(1) then
-		if(solid_tile(player.x+8, player.y) == false) then
-			player.x += player.speed
+		if(solid_tile(p.x+8, p.y) == false) then
+			p.x += p.speed
 		end
 	end
 
 	if btn(2) then
-		if(solid_tile(player.x, player.y-1) == false) then
-			player.y -= player.speed
+		if(solid_tile(p.x, p.y-1) == false) then
+			p.y -= p.speed
 		end
 	end
 	
 	if btn(3) then
-		if(solid_tile(player.x, player.y+8) == false) then
-			player.y += player.speed
+		if(solid_tile(p.x, p.y+8) == false) then
+			p.y += p.speed
 		end
 	end
 
 -- Movement audio and misc. actions related to movement
 	if btn(0) then
-		player.sprite = 1 
+		p.sprite = 1 
 		sfx(0)
 	end
 	
 	if btn(1) then
-		player.sprite = 2
+		p.sprite = 2
 		sfx(0)
 	end
 
@@ -59,7 +59,7 @@ end
 function _draw()					 
 	cls()
 	map(0,0,0,0,16,16)
-	spr(player.sprite, player.x, player.y)
+	spr(p.sprite, p.x, p.y)
 	spr(20,56,112)
 	spr(21,64,112)
 end
@@ -79,4 +79,8 @@ end
 
 function enemy()
 	spr(rnd(3)+17, rnd(112)+8, rnd(48))	
+	add(e,{
+			"hello",
+			})
+	print(e, 30, 122, 7)
 end
